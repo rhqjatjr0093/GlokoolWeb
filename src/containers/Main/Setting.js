9c1ad4f292a3748e9836178ac7287e358077a171
hyeseon-han <hyeseon405@gmail.com>
2021-02-15 17:37:06 +0900

    fix: 파일명수정

--- src/containers/Main/Setting.js
+++ src/containers/Main/Setting.js
@@ -1,32 +1,36 @@
-import React, { Component, useState } from 'react';
-import { makeStyles, useTheme, Text } from '@material-ui/core/styles';
-import { SettingContent, InputWithLabel, SettingButton, SettingDate, SettingGender, SettingError } from '../../components/Setting';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { auth, firestore } from '../../Firebase'
-import * as authActions from '../../redux/modules/Setting';
-import moment from 'moment';
-import { AuthWrapper } from '../../components/Auth'
+import React, { Component, useState } from "react";
+import { makeStyles, useTheme, Text } from "@material-ui/core/styles";
+import {
+  SettingContent,
+  InputWithLabel,
+  SettingButton,
+  SettingDate,
+  SettingGender,
+  SettingError,
+} from "../../components/Setting";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { auth, firestore } from "../../Firebase";
+import * as authActions from "../../redux/modules/Setting";
+import moment from "moment";
+import { AuthWrapper } from "../../components/Auth";
 
-
-
-class Setting extends Component {   
-    render() {
-        return (
-            <AuthWrapper>  
-            </AuthWrapper>
-        );
-    }
+class Setting extends Component {
+  render() {
+    return (
+      <AuthWrapper>
+        <p>수정화면</p>
+      </AuthWrapper>
+    );
+  }
 }
 const useStyles = makeStyles((theme) => ({
-    mainContainer: {
-        flexDirection: 'row',
-        
-    },
-    container: {
-        flex: 1
-    }
-
+  mainContainer: {
+    flexDirection: "row",
+  },
+  container: {
+    flex: 1,
+  },
 }));
 
 export default Setting;
